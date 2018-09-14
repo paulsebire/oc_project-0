01ad9dbad86cd92c15915f9350f4872184e23658
paul.sebire <psebire37@gmaul.com>
2018-09-14 10:56:38 +0200

    first commit: with 2 new products

--- src/main/java/com/lambazon/repository/ProductRepository.java
+++ src/main/java/com/lambazon/repository/ProductRepository.java
@@ -22,6 +22,8 @@ public class ProductRepository {
 		products.put(++id, new Product(id, 30, 69.99, "JVC HAFX8R Headphone", "Riptidz, In-Ear"));
 		products.put(++id, new Product(id, 40, 32.50, "VTech CS6114 DECT 6.0", "Cordless Phone"));
 		products.put(++id, new Product(id, 50, 895.00, "NOKIA OEM BL-5J", "Cell Phone "));
+		products.put(++id, new Product(id, -25, 32.50, "CAT 6M PO", "Inbreakable case")); //first product
+		products.put(++id, new Product(id, 5, 321654, "OntheSide", "support for phone ")); //second product
 	}
 	
 	public List<Product> products() {
